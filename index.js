9013c4be18b5cb936bbccf9409808b80615f9d08
Erick Vera <erickverac90@gmail.com>
2024-07-26 20:31:50 -0600

    Agregando metodos delete/deleteAll/post

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
-const koders = ["Erick", "Elvira", "Manuel", "Carlos", "Diana", "Fernanda"];
+let koders = ["Erick", "Elvira", "Manuel", "Carlos", "Diana", "Fernanda"];
 
 const server = express(); // Crea un servidor con express, tambien es comun que la gente lo llame app
+
+server.use(express.json()); // Middleware para que el servidor pueda leer json, es importante que este antes de las rutas, esta funcion se encarga de crear el body de la peticion
 
 // Ejercicio de muestra con koders
 server.get("/", (request, response) => {
@@ -16,6 +18,44 @@ server.get("/", (request, response) => {
 server.get("/koders", (request, response) => {
   response.json(koders);
   //response.status(500).json(koders); Asi le agregamos el status a la respuesta esperada especificando el codigo de respuesta
+});
+
+// Funcion para agregar koders
+server.post("/koders", (request, response) => {
+  console.log("post /koders");
+  console.log("body", request.body);
+
+  console.log("name", request.body.name);
+
+  const name = request.body.name;
+
+  // falsies: 0, "", null, undefined
+  if (!name) {
+    response.status(400).json({ message: "Name is required" });
+    return; // Para que no siga ejecutando el codigo
+  }
+
+  koders.push(name);
+
+  response.json(koders);
+});
+// Resetear la lista de koders
+//"/koders/:name" lo hacemos dinamico para que podamos borar un koder en especifico el uso de los ":"
+server.delete("/koders/:name", (request, response) => {
+  console.log("params", request.params); // Params es un objeto que contiene los parametros que se mandan en la url
+
+  const name = request.params.name;
+
+  const newKoders = koders.filter(
+    (koder) => koder.toLowerCase() !== name.toLowerCase()
+  ); // filter crea un nuevo arreglo
+  koders = newKoders; // Reasignamos el valor
+  response.json(koders);
+});
+// Borrar un koder
+server.delete("/koders", (request, response) => {
+  koders = [];
+  response.json(koders);
 });
 // Middleware, para escuchar al servidor
 server.listen(8080, () => {
